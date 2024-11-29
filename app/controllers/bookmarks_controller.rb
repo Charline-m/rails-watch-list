ae8aa9644c128bf2b42660a7cb88769e432f91d8
Charline Menant <mcharline407@hotmail.com>
2024-11-29 13:00:30 +0100

    done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,13 @@
 class BookmarksController < ApplicationController
 
-before_action :set_list
 
   def new
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
+
     @bookmark = Bookmark.new(bookmark_params)
       if @bookmark.save
       redirect_to list_path(@bookmark.list)
@@ -18,15 +19,10 @@ before_action :set_list
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy!
-    redirect_to bookmarks_path, status: :see_other
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
-
-  def set_list
-    @list = List.find(params[:list_id])
-
-  end
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :list_id, :movie_id)
